36904f94b9c5d9654d6a14aad8a5642243e62bea
rachaelcarroll <rachaelcarroll.m@gmail,com>
2021-04-23 17:48:04 -0400

    Layout player class file

---
+++ player.js
@@ -0,0 +1,32 @@
+class Player{
+  contructor(name, token, wins){
+    this.name = name;
+    this.token = token;
+    this.wins = wins;
+  }
+
+  takeTurn(){
+
+  }
+
+  saveWinsToStorage(){
+
+
+  }
+
+  retrieveWinsFromStorage(){
+
+    
+  }
+}
+
+
+
+
+
+// A player.js file that contains a Player class.
+// Player methods must include, but are not limited to:
+// constructor - properties should include: name (ex: 'hannah'), token (ex: '⭐️'), wins (ex: 0)
+// saveWinsToStorage
+// retrieveWinsFromStorage
+// takeTurn
