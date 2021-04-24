e007b9c187666f0f6f7a1edbe4ce32b48cf7c0a4
rachaelcarroll <rachaelcarroll.m@gmail,com>
2021-04-24 13:26:41 -0400

    Create chooseFighter func

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var classicGame = document.getElementById('classicGame');
 var magicGame = document.getElementById('magicGame');
 var middleHeader = document.getElementById('chooseGame');
 var gameContainer = document.getElementById('gameChoicesContainer');
-var gamePlaySection = document.getElementById('gamePlaySection');
+var chooseFighterSection = document.getElementById('chooseFighterSection');
 
 var game;
 
@@ -13,38 +13,11 @@ gameContainer.addEventListener('click', function(){
   playGame(event);
 });
 
+chooseFighterSection.addEventListener('click', function(){
+  chooseFighter(event);
+})
+
 //---------------FUNCTIONS------------------//
-
-function playGame(event){
-  if(event.target.id === 'classicGame'){
-    hide(classicGame);
-    hide(magicGame);
-    middleHeader.innerText = "Choose Your Classic Fighter!"
-    gamePlaySection.innerHTML += `
-    <img class='rock-fighter' id='rock' src='Assets/rock.png' alt='rock icon'>
-    <img class='paper-fighter' id='paper' src='Assets/post-it.png' alt='post-it icon'>
-    <img class='scissors-fighter' id='scissors' src='Assets/happy-scissors.png' alt='scissors icon'>
-    `
-    game = new Game('Classic');
-    game.chooseGame();
-    console.log(game)
-  }
-  if(event.target.id === 'magicGame'){
-    hide(classicGame);
-    hide(magicGame);
-    middleHeader.innerText = "Choose Your Magic Fighter!"
-    gamePlaySection.innerHTML += `
-    <img class='spell-fighter' id='spell' src='Assets/spell.png' alt='spell icon'>
-    <img class='voodoo-fighter' id='voodoo' src='Assets/voodoo-doll.png' alt='voodoo doll icon'>
-    <img class='crystals-fighter' id='crystals' src='Assets/crystals.png' alt='crystals icon'>
-    `
-    game = new Game('Magic');
-    game.chooseGame();
-    console.log(game);
-  }
-
-}
-
 
 function hide(element){
   element.classList.add('hidden');
@@ -52,4 +25,66 @@ function hide(element){
 
 function show(element){
   element.classList.remove('hidden')
+}
+
+function playGame(event){
+  if(event.target.id === 'classicGame'){
+    hide(classicGame);
+    hide(magicGame);
+    middleHeader.innerText = 'Choose Your Classic Fighter!'
+    chooseFighterSection.innerHTML += `
+    <img class='rock-fighter' id='rock' src='Assets/rock.png' alt='rock icon'>
+    <img class='paper-fighter' id='paper' src='Assets/post-it.png' alt='post-it icon'>
+    <img class='scissors-fighter' id='scissors' src='Assets/happy-scissors.png' alt='scissors icon'>
+    `
+    game = new Game('Classic');
+    game.chooseGame();
+  }
+  if(event.target.id === 'magicGame'){
+    hide(classicGame);
+    hide(magicGame);
+    middleHeader.innerText = 'Choose Your Magic Fighter!'
+  chooseFighterSection.innerHTML += `
+    <img class='spell-fighter' id='spell' src='Assets/spell.png' alt='spell icon'>
+    <img class='voodoo-fighter' id='voodoo' src='Assets/voodoo-doll.png' alt='voodoo doll icon'>
+    <img class='crystals-fighter' id='crystals' src='Assets/crystals.png' alt='crystals icon'>
+    `
+    game = new Game('Magic');
+    game.chooseGame();
+  }
+}
+
+function chooseFighter(event){
+  if(event.target.id === 'spell'){
+    game.humanFighter = 'spell';
+    hide(voodoo);
+    hide(crystals);
+    game.randomFighter();
+  } else if(event.target.id === 'voodoo'){
+    game.humanFighter = 'voodoo';
+    hide(spell);
+    hide(crystals);
+    game.randomFighter();
+  } else if(event.target.id === 'crystals'){
+    game.humanFighter = 'crystals'
+    hide(spell);
+    hide(voodoo);
+    game.randomFighter();
+  } else if(event.target.id === 'rock') {
+    game.humanFighter = 'rock';
+    hide(paper);
+    hide(scissors);
+    game.randomFighter();
+  } else if(event.target.id === 'paper'){
+    game.humanFighter = 'paper';
+    hide(rock);
+    hide(scissors);
+    game.randomFighter();
+  } else if(event.target.id === 'scissors'){
+    game.humanFighter = 'scissors';
+    hide(paper);
+    hide(rock);
+    game.randomFighter();
+    console.log(game);
+  }
 }
