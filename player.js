83890fc8c641236944b5e55cb7359794fb0661d2
rachaelcarroll <rachaelcarroll.m@gmail,com>
2021-04-25 18:21:40 -0400

    Remove pseudocode

--- player.js
+++ player.js
@@ -14,10 +14,3 @@ class Player{
     return wins;
  }
 }
-
-
-// A player.js file that contains a Player class.
-// Player methods must include, but are not limited to:
-// constructor - properties should include: name (ex: 'hannah'), token (ex: '⭐️'), wins (ex: 0)
-// saveWinsToStorage
-// retrieveWinsFromStorage
